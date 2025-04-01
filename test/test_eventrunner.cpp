e311075c688e814c6ea4eb2afa7bc86313e54214
Aaron Spiteri <azzmosphere@gmail.com>
2025-04-01 23:49:45 +1100

    test consumption events

--- test/test_eventrunner.cpp
+++ test/test_eventrunner.cpp
@@ -51,7 +51,7 @@ class MockConcreteHandler : public EventHandler {
 
 TEST_F(TestEventRunner, TestShouldExecute) {
     Environment env = createEnv();
-    RrQueueManager qmg = RrQueueManager(
+    RrQueueManager qmg(
         env.getQueues().getLimit(), 
         std::chrono::milliseconds(env.getQueues().getThreadWaitTime()), 
         std::chrono::milliseconds(env.getQueues().getThreadProcessTime()));
@@ -72,7 +72,7 @@ TEST_F(TestEventRunner, TestShouldExecute) {
 TEST_F(TestEventRunner, TestInvalidQueue) {
     MockConcreteHandler handler = MockConcreteHandler();
     Environment env = createEnv();
-    RrQueueManager qmg = RrQueueManager(
+    RrQueueManager qmg(
         env.getQueues().getLimit(), 
         std::chrono::milliseconds(env.getQueues().getThreadWaitTime()), 
         std::chrono::milliseconds(env.getQueues().getThreadProcessTime()));
@@ -86,7 +86,39 @@ TEST_F(TestEventRunner, TestInvalidQueue) {
     t.join();
     
     EXPECT_EQ(true, handler._reload_called);
+    delete(runner);
 }
+
+TEST_F(TestEventRunner, TestConsumeEvent) {
+    Environment env = createEnv();
+    RrQueueManager qmg(
+        env.getQueues().getLimit(), 
+        std::chrono::milliseconds(env.getQueues().getThreadWaitTime()), 
+        std::chrono::milliseconds(env.getQueues().getThreadProcessTime()));
+
+    StateManager smg;
+    MockConcreteHandler handler;
+    EXPECT_CALL(handler, consume(_, _)).Times(1).WillOnce(Return(true));
+
+    qmg.addQueue(RRP_QUEUES::AI_ENGINE);
+    qmg.addQueue(RRP_QUEUES::USER_INTERFACE);
+
+    msp_authkey* payload = new msp_authkey();
+    payload->set_key("test");
+    Event* event = new Event(MSP_AUTHKEY, MSPDIRECTION::EXTERNAL_IN, payload);
+    qmg.enqueue(RRP_QUEUES::AI_ENGINE, event);
+    EXPECT_EQ(false, qmg.isEmpty(RRP_QUEUES::AI_ENGINE));
+    EventRunner* runner = new EventRunner(&handler, qmg, smg, RRP_QUEUES::AI_ENGINE, RRP_QUEUES::USER_INTERFACE);
+    std::thread t = std::thread(EventRunner::run, runner);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    smg.setIsRunning(false);
+    t.join();
+
+    EXPECT_EQ(true, qmg.isEmpty(RRP_QUEUES::AI_ENGINE));
+
+    delete(runner);
+}
+
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
