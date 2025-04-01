7303a868627526bb29c4dd04bc8ef2e46391ddb0
Aaron Spiteri <azzmosphere@gmail.com>
2025-04-01 12:22:18 +1100

    added eventrunner

--- src/rrobot/events/eventrunner.cpp
+++ src/rrobot/events/eventrunner.cpp
@@ -9,7 +9,7 @@ void EventRunner::handleConsumeEvents(EventHandler* handler) {
         dlog_hnd << dlib::LDEBUG << "consume events available:  " << handler->name();
         Event* event = _qmg.dequeue(_inbound);
         _handler->consume(_smg, event);
-        delete(event);
+        delete (event);
     }
 }
 
@@ -24,15 +24,31 @@ void EventRunner::handleProduceEvents(EventHandler* handler) {
     }
 }
 
+void EventRunner::timeout(EventHandler* handler) {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    // arbitary pick of 3 second here, may be ut should be adjustable.
+    while (std::chrono::high_resolution_clock::now() - start < 3s) {
+        if (handler->getStatus() == RRP_STATUS::ACTIVE) {
+            break;
+        }
+    }
+
+    dlog_hnd << dlib::LFATAL << " timeout error occrred while waiting for " << handler->name();
+    throw new TimeOutException("timeout error occrred in event runner");
+}
+
+// TODO: This method could do with some timeouts.
 void EventRunner::run(EventRunner& runner) {
-    EventHandler* handler =  runner._handler;
+    EventHandler* handler = runner._handler;
     dlog_hnd << dlib::LDEBUG << "initializing:  " << handler->name();
     runner._smg.setStatus(RRP_STATUS::INITILIZING);
 
     handler->setUp();
-    while(handler->getStatus() != RRP_STATUS::ACTIVE);
+    // set a timeout to become active
+    runner.timeout(handler);
 
-    dlog_hnd << dlib::LDEBUG <<  handler->name() << " is now active";
+    dlog_hnd << dlib::LDEBUG << handler->name() << " is now active";
     runner._smg.setStatus(RRP_STATUS::ACTIVE);
 
     // isRunning can be considered a kill switch if it is hit, then stop
@@ -44,29 +60,31 @@ void EventRunner::run(EventRunner& runner) {
                 runner.handleConsumeEvents(handler);
             }
         } catch (const std::exception& e) {
-            dlog_hnd << dlib::LERROR << "handler: " <<  handler->name() << "reported :" << "error occured while handling event: " << e.what();
+            dlog_hnd << dlib::LERROR << "handler: " << handler->name()
+                     << "reported :" << "error occured while handling event: " << e.what();
             runner._smg.setStatus(RRP_STATUS::ERROR);
             handler->onError(e);
         }
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::microseconds elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-        std::chrono::microseconds waitTime = 
-            std::chrono::duration_cast<std::chrono::microseconds>(elapsed - runner._qmg.queueProcessTime(runner._inbound));
+        std::chrono::microseconds waitTime = std::chrono::duration_cast<std::chrono::microseconds>(
+            elapsed - runner._qmg.queueProcessTime(runner._inbound));
 
         if (waitTime.count() > 0) {
-            dlog_hnd << dlib::LDEBUG << handler->name() << " finished early now waiting for " << waitTime.count() << "msecs";
+            dlog_hnd << dlib::LDEBUG << handler->name() << " finished early now waiting for " << waitTime.count()
+                     << "msecs";
             this_thread::sleep_for(waitTime);
         }
 
         if (handler->getStatus() != RRP_STATUS::ACTIVE) {
-            dlog_hnd << dlib::LWARN << "handler" <<  handler->name() << ": handler status was not active attempting to reload";
+            dlog_hnd << dlib::LWARN << "handler" << handler->name()
+                     << ": handler status was not active attempting to reload";
             handler->reload();
-            while(handler->getStatus() != RRP_STATUS::ACTIVE);
+            runner.timeout(handler);
         }
     }
     runner._smg.setStatus(RRP_STATUS::SHUTTING_DOWN);
     handler->tearDown();
-    while(handler->getStatus() != RRP_STATUS::TERMINATED);
+    while (handler->getStatus() != RRP_STATUS::TERMINATED);
     runner._smg.setStatus(RRP_STATUS::TERMINATED);
 }
-
