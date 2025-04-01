fa1867e5ee48061388e6070d15a071c2fdc476b6
Aaron Spiteri <azzmosphere@gmail.com>
2025-04-01 22:20:07 +1100

    Created eventrunner and handler (tested)

--- src/rrobot/events/eventrunner.cpp
+++ src/rrobot/events/eventrunner.cpp
@@ -24,51 +24,50 @@ void EventRunner::handleProduceEvents(EventHandler* handler) {
     }
 }
 
-void EventRunner::timeout(EventHandler* handler) {
+void EventRunner::timeout(EventHandler* handler, RRP_STATUS wanted) {
     auto start = std::chrono::high_resolution_clock::now();
 
     // arbitary pick of 3 second here, may be ut should be adjustable.
     while (std::chrono::high_resolution_clock::now() - start < 3s) {
-        if (handler->getStatus() == RRP_STATUS::ACTIVE) {
-            break;
+        if (handler->getStatus() == wanted) {
+            return;
         }
     }
 
     dlog_hnd << dlib::LFATAL << " timeout error occrred while waiting for " << handler->name();
-    throw new TimeOutException("timeout error occrred in event runner");
+    throw new TimeOutException("timeout error occurred in event runner: expected");
 }
 
-// TODO: This method could do with some timeouts.
-void EventRunner::run(EventRunner& runner) {
-    EventHandler* handler = runner._handler;
+void EventRunner::run(EventRunner* runner) {
+    EventHandler* handler = runner->_handler;
     dlog_hnd << dlib::LDEBUG << "initializing:  " << handler->name();
-    runner._smg.setStatus(RRP_STATUS::INITILIZING);
+    runner->_smg.setStatus(RRP_STATUS::INITILIZING);
 
     handler->setUp();
     // set a timeout to become active
-    runner.timeout(handler);
+    runner->timeout(handler, RRP_STATUS::ACTIVE);
 
     dlog_hnd << dlib::LDEBUG << handler->name() << " is now active";
-    runner._smg.setStatus(RRP_STATUS::ACTIVE);
+    runner->_smg.setStatus(RRP_STATUS::ACTIVE);
 
     // isRunning can be considered a kill switch if it is hit, then stop
-    while (runner._smg.isRunning()) {
+    while (runner->_smg.isRunning()) {
         auto start = std::chrono::high_resolution_clock::now();
         try {
             if (handler->getStatus() == RRP_STATUS::ACTIVE) {
-                runner.handleProduceEvents(handler);
-                runner.handleConsumeEvents(handler);
+                runner->handleProduceEvents(handler);
+                runner->handleConsumeEvents(handler);
             }
         } catch (const std::exception& e) {
             dlog_hnd << dlib::LERROR << "handler: " << handler->name()
                      << "reported :" << "error occured while handling event: " << e.what();
-            runner._smg.setStatus(RRP_STATUS::ERROR);
+            runner->_smg.setStatus(RRP_STATUS::ERROR);
             handler->onError(e);
         }
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::microseconds elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
         std::chrono::microseconds waitTime = std::chrono::duration_cast<std::chrono::microseconds>(
-            elapsed - runner._qmg.queueProcessTime(runner._inbound));
+            elapsed - runner->_qmg.queueProcessTime(runner->_inbound));
 
         if (waitTime.count() > 0) {
             dlog_hnd << dlib::LDEBUG << handler->name() << " finished early now waiting for " << waitTime.count()
@@ -80,11 +79,18 @@ void EventRunner::run(EventRunner& runner) {
             dlog_hnd << dlib::LWARN << "handler" << handler->name()
                      << ": handler status was not active attempting to reload";
             handler->reload();
-            runner.timeout(handler);
+            runner->timeout(handler, RRP_STATUS::ACTIVE);
         }
     }
-    runner._smg.setStatus(RRP_STATUS::SHUTTING_DOWN);
-    handler->tearDown();
-    while (handler->getStatus() != RRP_STATUS::TERMINATED);
-    runner._smg.setStatus(RRP_STATUS::TERMINATED);
+    try {
+        runner->_smg.setStatus(RRP_STATUS::SHUTTING_DOWN);
+        handler->tearDown();
+        runner->timeout(handler, RRP_STATUS::TERMINATED);
+        runner->_smg.setStatus(RRP_STATUS::TERMINATED);
+    } catch (const std::exception& e) {
+        dlog_hnd << dlib::LERROR << "handler: " << handler->name()
+            << "reported :" << "error occured while shutting down: " << e.what();
+        runner->_smg.setStatus(RRP_STATUS::ERROR);
+        handler->onError(e);
+    }
 }
---
+++ test/test_eventrunner.cpp
@@ -0,0 +1,70 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <filesystem>
+#include <fstream>
+#include <rrobot/environment/environmentProcessor.hpp>
+#include <rrobot/events/eventrunner.hpp>
+
+using ::testing::_;
+using ::testing::Return;
+namespace fs = std::filesystem;
+using namespace rrobot;
+
+class TestEventRunner : public ::testing::Test {
+   protected:
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
+Environment createEnv() {
+    const fs::path filepath = "manifests/virtual.json";
+    std::ifstream ifs(filepath);
+    json manifest = json::parse(ifs);
+    ifs.close();
+
+    return EnviromentProcessor::createEnvironment(manifest);
+}
+
+class MockConcreteHandler : public EventHandler {
+   public:
+    MockConcreteHandler() {}
+    ~MockConcreteHandler() {}
+    MOCK_METHOD(bool, available, (), (override));
+    MOCK_METHOD(bool, consume, (StateManager&, Event*), (override));
+    MOCK_METHOD(Event*, produce, (StateManager&), (override));
+
+    string name() override { return "mockhandler"; }
+    void init(StateManager& smg, Environment& env) {}
+    void tearDown() override {
+        _status = RRP_STATUS::TERMINATED;
+    }
+    RRP_STATUS getStatus() override {return _status;}
+
+    RRP_STATUS _status = RRP_STATUS::ACTIVE;
+};
+
+TEST_F(TestEventRunner, TestShouldExecute) {
+    Environment env = createEnv();
+    RrQueueManager qmg = RrQueueManager(
+        env.getQueues().getLimit(), 
+        std::chrono::milliseconds(env.getQueues().getThreadWaitTime()), 
+        std::chrono::milliseconds(env.getQueues().getThreadProcessTime()));
+
+    StateManager smg;
+    MockConcreteHandler handler;
+    qmg.addQueue(RRP_QUEUES::AI_ENGINE);
+    qmg.addQueue(RRP_QUEUES::USER_INTERFACE);
+    EventRunner* runner = new EventRunner(&handler, qmg, smg, RRP_QUEUES::AI_ENGINE, RRP_QUEUES::USER_INTERFACE);
+    std::thread t = std::thread(EventRunner::run, runner);
+    std::this_thread::sleep_for(std::chrono::milliseconds(env.getQueues().getThreadProcessTime()));
+    smg.setIsRunning(false);
+    t.join();
+    EXPECT_EQ(true, true);
+    delete(runner);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
