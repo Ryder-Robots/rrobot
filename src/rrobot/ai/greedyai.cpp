81aea5c89555a90c3e0cbc6b879677df59e861bc
Aaron Spiteri <azzmosphere@gmail.com>
2025-05-28 18:02:00 +1000

    Refactor GreedyAi: Add rounding function and improve transverse logic

--- src/rrobot/ai/greedyai.cpp
+++ src/rrobot/ai/greedyai.cpp
@@ -4,8 +4,10 @@ using namespace rrobot;
 
 /**
  * TODO:
- * - add stop command.
  * - some logging is desperately needed.
+ * - c should indicate vector.
+ * - cp needs to be added to simply equations,  this is just cp = cp + n after movement, then round to get actual point.
+ * - need to round off very small numbers by using a wrapper for cos, and sin
  */
 
 /*
@@ -15,36 +17,39 @@ using namespace rrobot;
  * n = vector of travel, including heading.
  */
 PSTATE GreedyAi::transverse(const dlib::vector<float, VECTOR_DIM> dp) {
-
     // call decto first, this will set the sensors.
     detecto();
 
     const dlib::vector<float, VECTOR_DIM> sp = _sm.getCp();
-    dlib::vector<float, VECTOR_DIM> c = dp - sp;
+    dlib::vector<float, VECTOR_DIM> c = dp - _sm.getFeatures().get_sensor_mag().get_data();
     // previously transversed points
     std::stack<dlib::vector<float, VECTOR_DIM>> _trans;
-    // local exclusions,  these include all paths that have been transversed. 
+    // local exclusions,  these include all paths that have been transversed.
     std::vector<dlib::vector<float, VECTOR_DIM>> excl = _excl;
 
-    // resolutions need to phased out in future versions, this can be calculated by using SIN and 
-    // vector maths for more accurate turning. It assumes that everything is 45 degrees, which is obviously
-    // not correct.
     while (c.length() > 0) {
         bool f = false;
-        dlib::vector<float, VECTOR_DIM> n(0,0,0);  // next transversal
-        float l = c.length();
+        float r = R(atan2(c.y(), c.x())), l = c.length();
 
-        // attempt to get best path first,
-        for (auto v : res) {
-            // n is a point at this stage
-            dlib::vector<float, VECTOR_DIM> nd = c - v;
-            if (is_valid((dp + nd) + sp, excl)) {
-                if (!f || nd.length() < l) {
-                    l = nd.length();
-                    n = v;
-                    f = true;
-                } 
+        // next transversal TODO: set a formula that will round really low precisions, less that 0.00
+        dlib::vector<float, VECTOR_DIM> n(R(cos(r)), R(sin(r)), 0);
+
+        // TODO: should simplify this calculation.
+        if (!is_valid(sp + c + n, excl)) {
+            // attempt to get best path first,
+            for (auto v : res) {
+                // n is a point at this stage
+                dlib::vector<float, VECTOR_DIM> nd = c - v;
+                if (is_valid((dp + nd) + sp, excl)) {
+                    if (!f || nd.length() < l) {
+                        l = nd.length();
+                        n = v;
+                        f = true;
+                    }
+                }
             }
+        } else {
+            f = true;
         }
 
         if (f) {
@@ -75,30 +80,27 @@ PSTATE GreedyAi::transverse(const dlib::vector<float, VECTOR_DIM> dp) {
     return PSTATE::P_AVAILABLE;
 }
 
-/*
- * use base algorithm rotate the robot. 
- */
-void GreedyAi::rotate(dlib::vector<float, VECTOR_DIM> v) {
-    _base.rotate(v);
+ // round to 4 decimal place, by converting to an integer then dividing the number by 10000
+float GreedyAi::R(float x) {
+    return std::round(x * 10000.f) / 10000.0f;
 }
+
+/*
+ * use base algorithm rotate the robot.
+ */
+void GreedyAi::rotate(dlib::vector<float, VECTOR_DIM> v) { _base.rotate(v); }
 
 /*
  * use base algorithm to detect object.
  */
-bool GreedyAi::detecto() { 
-    return _base.detecto(); 
-}
+bool GreedyAi::detecto() { return _base.detecto(); }
 
 /*
  * use base algorithm to move
  */
-void GreedyAi::move(dlib::vector<float, VECTOR_DIM> v) {
-    _base.move(v);
-}
+void GreedyAi::move(dlib::vector<float, VECTOR_DIM> v) { _base.move(v); }
 
-void GreedyAi::stop() {
-    _base.stop();
-}
+void GreedyAi::stop() { _base.stop(); }
 
 /*
  * compare attributes within all excluded points, to ensure that point has not been discovered before.
